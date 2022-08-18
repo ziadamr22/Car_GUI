1182085b1b20dcdaed6eca30be5cad8d55744d3f
NadaMamdouh225 <105312001+NadaMamdouh225@users.noreply.github.com>
2022-08-18 14:14:11 +0200

    Motor direction function was modified

--- megaProject_01/megaProject_01.ino
+++ megaProject_01/megaProject_01.ino
@@ -3,9 +3,12 @@
 #define GREEN_PIN            10    //PWM pin
 #define BLUE_PIN             9     //PWM pin
 #define Enable_Pin           3     //PWM pin
-#define C_Motor_dir_A1       2
-#define C_Motor_dir_A2       4
-#define C_Motor_Speed_A3     3     //PWM pin
+#define C_Motor1_dir_A1      2
+#define C_Motor1_dir_A2      4
+#define C_Motor2_dir_A1      7
+#define C_Motor2_dir_A2      8 
+#define C_Motor1_Speed_A3    3     //PWM pin
+#define C_Motor2_Speed_A3    6      //PWM pin   
 #define pot                  A0
 //#define C_Motor_pot          A1 
 #define joyX                 A2
@@ -18,7 +21,6 @@
 int MotorSpeed;
 int X_value,Y_value;
 int pwmOutput;
-
 char key;
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -36,12 +38,18 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );  // st
 
 void setup() {
   Serial.begin(9600);
+  // initializing indicator pins
   pinMode(RED_PIN,OUTPUT);
   pinMode(GREEN_PIN,OUTPUT);
   pinMode(BLUE_PIN,OUTPUT);
-  pinMode(C_Motor_dir_A1,OUTPUT);
-  pinMode(C_Motor_dir_A2,OUTPUT);
-  pinMode(C_Motor_Speed_A3,OUTPUT);
+  // initializing Motor direction pins
+  pinMode(C_Motor1_dir_A1,OUTPUT);
+  pinMode(C_Motor1_dir_A2,OUTPUT);
+  pinMode(C_Motor2_dir_A1,OUTPUT);
+  pinMode(C_Motor2_dir_A2,OUTPUT);
+  // initializing Motor speed pins
+  pinMode(C_Motor1_Speed_A3,OUTPUT);
+  pinMode(C_Motor2_Speed_A3,OUTPUT);
   
 
   
@@ -52,12 +60,32 @@ void loop() {
   //Y_value = analogRead(joyY);
    //pwmOutput = analogRead(pot); 
    //pwmOutput = map(pwmOutput, 0, 1023, 0 , 255); 
-   //analogWrite(C_Motor_Speed_A3, pwmOutput); // Send PWM signal to L298N Enable pin
+   //analogWrite(C_Motor1_Speed_A3, pwmOutput); // Send PWM signal to L298N Enable pin
    
    key = keypad.getKey();
    if (key) 
-   { 
-    SetMotor_Dir(key);
+   {  
+      if(key == '2')
+      {
+        Forward();
+      }
+      else if(key == '8')
+      {
+        Backward(); 
+      }
+      else if(key == '6')
+      {
+        Right();
+      }
+      else if(key == '4')
+      {
+        Left(); 
+      }
+      else if(key == '5')
+      {
+        Stop();
+      }
+    
    }
    
   MotorSpeed = analogRead(pot);
@@ -69,9 +97,13 @@ void loop() {
 }
 
 
+
+
+
+
+
 void RGB_SpeedIndicator(int MotorSpeed)
 { 
-  //MotorSpeed = map(MotorSpeed,0,1023,0,2);
   if(MotorSpeed == 0)
   {
     analogWrite(GREEN_PIN,TurnON_RGB); 
@@ -100,41 +132,71 @@ int SetMotor_Speed(int Pot_Value)
   Speed = map(Pot_Value,0,1023,0,2);
   if(Speed == 0)
   {
-    analogWrite(C_Motor_Speed_A3,Low_Speed);
+    analogWrite(C_Motor1_Speed_A3,Low_Speed);
+    analogWrite(C_Motor2_Speed_A3,Low_Speed);
   }
   else if(Speed == 1)
   {
-    analogWrite(C_Motor_Speed_A3,Med_Speed);
+    analogWrite(C_Motor1_Speed_A3,Med_Speed);
+    analogWrite(C_Motor2_Speed_A3,Med_Speed);
   }
   
   else if (Speed == 2)
   {
-    analogWrite(C_Motor_Speed_A3,High_Speed);
+    analogWrite(C_Motor1_Speed_A3,High_Speed);
+    analogWrite(C_Motor2_Speed_A3,High_Speed);
   }
   
   return Speed;
 }
 
-void SetMotor_Dir(char key)
-{
+//void SetMotor_Dir(char key)
+//{
     //X_value = map(X_value,0,1023,0,3);                  here,will replace joystick with keypad and below condition based on keypad    
     //Y_value = map(Y_value,0,1023,0,3);
-    // by pressing 2 using the keypad in protous motor moves backward   
-    if (key == '2' ) {                                                 // DOWN means moving backward
-    digitalWrite(C_Motor_dir_A1, HIGH);
-    digitalWrite(C_Motor_dir_A2, LOW);
+//}
+
+void Forward()
+{                             
+    digitalWrite(C_Motor1_dir_A1, HIGH);        //  Motor1 moves forward
+    digitalWrite(C_Motor1_dir_A2, LOW);
+    digitalWrite(C_Motor2_dir_A1, HIGH);        //  Motor2 moves forward
+    digitalWrite(C_Motor2_dir_A2, LOW);
     delay(20);
-    }
-    // by pressing 8 using the keypad in protous motor moves forward
-    if (key == '8' ) {                                                // UP means moving forward
-    digitalWrite(C_Motor_dir_A1, LOW);
-    digitalWrite(C_Motor_dir_A2, HIGH);
+}
+
+void Backward()
+{                                              
+    digitalWrite(C_Motor1_dir_A1, LOW);          //  Motor1 moves backward
+    digitalWrite(C_Motor1_dir_A2, HIGH);
+    digitalWrite(C_Motor2_dir_A1, LOW);          //  Motor2 moves backward
+    digitalWrite(C_Motor2_dir_A2, HIGH);
     delay(20);
-   }
-   // u should press 7 on protous keypad to stop motion
-   if (key == '1' ) {                                                // to stop motion
-    digitalWrite(C_Motor_dir_A1, LOW);
-    digitalWrite(C_Motor_dir_A2, LOW);
+}
+
+void Right()
+{                                              
+    digitalWrite(C_Motor1_dir_A1, LOW);            //  Motor1 moves backward
+    digitalWrite(C_Motor1_dir_A2, HIGH);
+    digitalWrite(C_Motor2_dir_A1, HIGH);           //  Motor2 moves forward
+    digitalWrite(C_Motor2_dir_A2, LOW);
     delay(20);
-   }
+}
+
+void Left()
+{                                               
+    digitalWrite(C_Motor1_dir_A1, HIGH);          //  Motor1 moves forward     
+    digitalWrite(C_Motor1_dir_A2, LOW);          
+    digitalWrite(C_Motor2_dir_A1, LOW);           //  Motor2 moves backward
+    digitalWrite(C_Motor2_dir_A2, HIGH);
+    delay(20);
+}
+
+void Stop()
+{                             
+    digitalWrite(C_Motor1_dir_A1, LOW);          // to stop motion
+    digitalWrite(C_Motor1_dir_A2, LOW);
+    digitalWrite(C_Motor2_dir_A1, LOW);
+    digitalWrite(C_Motor2_dir_A2, LOW);
+    delay(20);
 }
